b5d73ee7af0d2fda1c02d44073a1be97d46e58bc
Aliraza786789 <lookatme786.ar@gmail.com>
2020-09-05 16:00:54 +0500

    Randomization

---
+++ Assets/PanningValueInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanningValueInitializer : MonoBehaviour
+{
+    
+    void Start()
+    {
+        for(int i= 0; i<=InventoryManager.Instance.bowlsManager.activeBowlsIndexes.Length-1;i++) 
+        {
+                this.transform.GetChild(i).GetComponent<Triggers>().DefaultValueOfPanning=
+                InventoryManager.Instance.allBowls[InventoryManager.Instance.bowlsManager.
+                activeBowlsIndexes[i]].GetComponent<AudioSource>().panStereo;
+        }
+    }
+}
---
+++ Assets/Triggers.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triggers : MonoBehaviour
+{
+    public float DefaultValueOfPanning;
+    void Start()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Bowl")
+        other.GetComponent<AudioSource>().panStereo = DefaultValueOfPanning;
+    }
+
+}
